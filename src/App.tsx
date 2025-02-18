249bb749f8ba1140b83c719b1e81a4dc45897aea
tsumo <tsumo@users.noreply.github.com>
2025-02-18 22:18:27 +0300

    feat: sotillo crew

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { CronusCrew } from "./components/sheets/CronusCrew";
 import { Equipment } from "./components/sheets/Equipment";
 import { MonteroAgendas } from "./components/sheets/MonteroAgendas";
 import { MonteroCrew } from "./components/sheets/MonteroCrew";
+import { SotilloCrew } from "./components/sheets/SotilloCrew";
 
 export const App = () => {
   return (
@@ -10,6 +11,7 @@ export const App = () => {
       <MonteroCrew />
       <MonteroAgendas />
       <CronusCrew />
+      <SotilloCrew />
     </>
   );
 };
